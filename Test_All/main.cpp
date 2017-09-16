904c56a2695fb319218c444487b7f3e2391ccc2f
SofyaTorosyan <sofya.torosyan.96@mail.ru>
2017-09-16 17:24:16 +0400

    Added main() to Test_All.

---
+++ Test_All/main.cpp
@@ -0,0 +1,23 @@
+#include "../Header.h"
+
+int main()
+{
+    int    x = 0;
+    int    y;
+    while (cin.get() != '0')
+    {
+        Timer T;
+        y = 0;
+
+        //  hash_dot.Random_Generate ();
+        hash_dot.Read_From_File();
+        m.Create_Vertexes();
+        //  m.enumerateBuildings();
+        //  m.Circle();
+        //  m.print ();
+        cout << "------------------------- \nCount of bridges is " << count_of_bridges << "\n-------------------------" << endl;
+        clean_all();
+        cout << hash_dot.length() << " " << hash_dot.width() << endl;
+    }
+    std::system("pause");
+}
