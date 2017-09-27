80c6d1d96a69f5247331fef29c63dd622bae2d8e
SofyaTorosyan <sofya.torosyan.96@mail.ru>
2017-09-28 00:06:22 +0400

    Added strip_whitespace(...).

--- Test_All/main.cpp
+++ Test_All/main.cpp
@@ -1,49 +1,65 @@
 #include "../Header.h"
 
+std::string strip_whitespaces(std::string s)
+{
+    s.erase(std::remove_if(s.begin(), s.end(), [](char c) { return std::isspace(c); }), s.end());
+    return s;
+}
+
 bool files_identical(std::string fileNameA, std::string fileNameB)
 {
     std::ifstream A(fileNameA);
     std::ifstream B(fileNameB);
 
+    if (!A)
+        cout << "Not A";
+    if (!B)
+        cout << "Not B";
+    
     std::stringstream a;
     std::stringstream b;
 
     a << A.rdbuf();
-    b << B.rdbuf();
+    b << B.rdbuf(); 
 
-    return (a.str() == b.str());
-}
+    string astr = a.str();  
+    string bstr = b.str(); 
 
-void test_range(int a, int b)
+  //  auto pos = mismatch(astr.begin(), astr.end(), bstr.begin());
+    return (strip_whitespaces(a.str()) == strip_whitespaces(b.str()));
+} 
+ 
+
+void test_range(int a, int b) 
 {
-    const std::string dirHDGold = "../Tests/Hashdots_gold/";
-    const std::string dirConnected = "../Tests/Connected/";
-    const std::string dirConnectedGold = "../Tests/Connected_gold/";
+    const std::string dirHD_10_10        = "../Tests/Hashdots_10_10/";
+    const std::string dirConnected       = "../Tests/Connected/";
+    const std::string dirConnected_10_10 = "../Tests/Connected_10_10/";
 
     for (int i = a; i < b; i++)
     {
-        const std::string HDFile = dirHDGold + "model_hashdot_" + std::to_string(i) + ".txt";  // ??????
-        const std::string CFile  = dirConnected + "model_connected_" + std::to_string(i) + ".txt";
-        const std::string CGFile = dirConnectedGold + "model_connected_" + std::to_string(i) + ".txt";
-        hash_dot.Read_From_File(HDFile); // Reads from ../Tests/Hashdots_gold/model_hash_dot_0 file...
+        const std::string HDFile = dirHD_10_10 + "model_hashdot_" + std::to_string(i) + ".txt";  
+        const std::string B  = dirConnected + "model_connected_" + std::to_string(i) + ".txt";
+        const std::string A  = dirConnected_10_10 + "model_connected_" + std::to_string(i) + ".txt";
+        hash_dot.Read_From_File(HDFile);
         m.Create_Vertexes();
         m.enumerateBuildings();
-        m.Circle();
+        m.Circle(); 
 
-        ofstream C_File;
-        C_File.open(CFile);
-        m.Write_To_File(C_File);
+        m.Write_To_File(B); 
 
-        if (!files_identical(CGFile, CFile))
+        if (!files_identical(A, B))
+        {
             cout << "Not Identical";
+            break;
+        }
         else
             cout << "OK";
-        // m.print();
-    }
+    } 
 }
 
 int main()
 {
-    test_range(0, 2000);   
+    test_range(4, 5);
     std::system("pause");
-}
+} 
