88a62ad1020a3ab6259753928666efae74ba49e7
SofyaTorosyan <sofya.torosyan.96@mail.ru>
2017-11-16 02:24:15 +0400

    Removed inline s.

--- HashDot.cpp
+++ HashDot.cpp
@@ -6,7 +6,7 @@ HashDot::HashDot(int l, int w) : length_(l), width_(w)
 HashDot::HashDot() : length_(30), width_(30)
 { }
 
-inline void HashDot::clear()
+ void HashDot::clear()
 {
     hash_dot_.clear();
 }
@@ -19,53 +19,53 @@ inline void HashDot::print()
     cout << endl;
 }
 
-inline int HashDot::width()
+ int HashDot::width()
 {
     return width_;
 }
 
-inline int HashDot::length()
+ int HashDot::length()
 {
     return length_;
 }
 
-inline int HashDot::row_size()
+ int HashDot::row_size()
 {
     return length_ + 1;
 }
 
-inline int HashDot::col_size()
+ int HashDot::col_size()
 {
     return width_ + 1;
 }
 
-inline char HashDot::up(int x, int y)
+ char HashDot::up(int x, int y)
 {
     if (y == 0)
         return '.';
     return hash_dot_[y - 1][x];
 }
 
-inline char HashDot::left(int x, int y)
+ char HashDot::left(int x, int y)
 {
     if (x == 0)
         return '.';
     return hash_dot_[y][x - 1];
 }
 
-inline char HashDot::upleft(int x, int y)
+ char HashDot::upleft(int x, int y)
 {
     if (y == 0 || x == 0)
         return '.';
     return hash_dot_[y - 1][x - 1];
 }
 
-inline char& HashDot::operator()(int i, int j)
+ char& HashDot::operator()(int i, int j)
 {
     return  hash_dot_[j][i];
 }
 
-inline void HashDot::Random_Generate()
+ void HashDot::Random_Generate()
 {
     cout << "City with random hashdots\n";
     for (int j = 0; j < width_; j++)
@@ -76,7 +76,7 @@ inline void HashDot::Random_Generate()
     }
 }
 
-inline void HashDot::Read_From_File(const std::string fileName = "My_City.txt")
+ void HashDot::Read_From_File(const std::string fileName = "My_City.txt")
 {
     hash_dot_.clear();
     std::ifstream fileIn(fileName);
@@ -94,7 +94,7 @@ inline void HashDot::Read_From_File(const std::string fileName = "My_City.txt")
     width_ = hash_dot_.size();
 }
 
-inline string& HashDot::Random_Generate_String(std::string& line)
+ string& HashDot::Random_Generate_String(std::string& line)
 {
     char str[20] = { '.','.','.','.','#','.','.','.','.','.' ,'.','.','.','.','.' ,'.','.','.','.','.' };
     for (int i = 0; i < length_; i++)
