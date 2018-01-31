f58e966e08f1e186edae6d328c33d3e9523dbcf2
SofyaTorosyan <sofya.torosyan.96@mail.ru>
2018-01-31 13:07:50 +0400

    (CppCheck)Removed useless x,y.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,12 @@
 #include"Header.h"
 int main()
 {
-    int    x = 0;
-    int    y;
      //while (cin.get() != '0')
-     //{
+     {
         Timer T;
-        y = 0;
         const std::string fileName = "My_City.txt";
-     // m.hash_dot.Random_Generate ();
-        m.hash_dot.Read_From_File(fileName);
+        m.hash_dot.Random_Generate ();
+      //m.hash_dot.Read_From_File(fileName);
         m.Create_Vertexes();
         m.enumerateBuildings(); 
         m.Circle(); 
@@ -18,9 +15,9 @@ int main()
         cout << "Total length......... "                          << bridge_length    << endl;
         cout << "Disconnected groups.. "                          << non_connected_buildings << "\n----------------------- \n";
         clean_all();
-  //  }
-    std::system("pause");
-}
+    }
+     std::system("pause"); 
+} 
 
 
 
